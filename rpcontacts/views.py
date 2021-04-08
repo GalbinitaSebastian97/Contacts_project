8b1d8a5be2586743b1f0f837536651301863c233
GalbinitaSebastian97 <galbinita.sebastian@yahoo.com>
2021-04-08 18:12:33 +0300

    Created the GUI of yhe contact book using Python and PyQt

--- rpcontacts/views.py
+++ rpcontacts/views.py
@@ -3,6 +3,10 @@
 """This modul provides views to manage the contacts table"""
 
 from PyQt5.QtWidgets import (
+    QVBoxLayout,
+    QAbstractItemView,
+    QPushButton,
+    QTableView,
     QHBoxLayout,
     QMainWindow,
     QWidget,
@@ -12,11 +16,36 @@ class Window(QMainWindow):
     """Main Window"""
     def __init__(self, parent = None):
         """Initializer"""
-        super().__init__(parent)
+        super().__init__(parent) 
         self.setWindowTitle("Contacts")
         self.resize(550,250)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
-        self.layout = QHBoxLayout()
+        self.layout = QHBoxLayout ()
         self.centralWidget.setLayout(self.layout)
+        self.setupUI()
+
+    def setupUI(self):
+        """Setup the main winsow's GUI."""
+        #Create the table viw widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+
+        #Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+
+        #Layout the GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        layout.addWidget(self.table)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
+
+
         
